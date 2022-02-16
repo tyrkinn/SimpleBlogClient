60cfc621fe8794a90c76c4b916a760dbd49330d1
tyrkinn <nikemad61@gmail.com>
2022-02-17 00:22:55 +0300

    Add ResponsiveSidebar component

---
+++ src/components/sidebar/ResponsiveSidebar.tsx
@@ -0,0 +1,36 @@
+import {LinkItemProps, SidebarContent} from './SidebarContent';
+import {Drawer, DrawerContent} from '@chakra-ui/react';
+import React, {Fragment} from 'react';
+
+export interface ResponsiveSidebarProps {
+  links: LinkItemProps[];
+  logo: string;
+  onClose: () => void;
+  isOpen: boolean;
+}
+
+export const ResponsiveSidebar: React.FC<ResponsiveSidebarProps> =
+  ({links, logo, onClose, isOpen}) => {
+    return (
+      <Fragment>
+        <SidebarContent
+          links={links}
+          logo={logo}
+          onClose={() => onClose}
+          display={{base: 'none', md: 'block'}}
+        />
+        <Drawer
+          autoFocus={false}
+          isOpen={isOpen}
+          placement="left"
+          onClose={onClose}
+          returnFocusOnClose={false}
+          onOverlayClick={onClose}
+          size="full">
+          <DrawerContent>
+            <SidebarContent logo={logo} onClose={onClose} links={links}/>
+          </DrawerContent>
+        </Drawer>
+      </Fragment>
+    );
+  };
