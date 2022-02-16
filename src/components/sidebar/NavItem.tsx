c30da096d86e25c5283e0dd6945eeebdd4f32fbd
tyrkinn <nikemad61@gmail.com>
2022-02-17 00:20:18 +0300

    Add NavItem component

---
+++ src/components/sidebar/NavItem.tsx
@@ -0,0 +1,43 @@
+import {Flex, FlexProps, Icon, Link} from '@chakra-ui/react';
+import React, {ReactText} from 'react';
+import {IconType} from 'react-icons';
+
+export interface NavItemProps extends FlexProps {
+  icon: IconType;
+  children: ReactText;
+}
+
+export const NavItem = ({icon, children, ...rest}: NavItemProps) => {
+  return (
+    <Link
+      href="#"
+      style={{textDecoration: 'none'}}
+      _focus={{boxShadow: 'none'}}
+    >
+      <Flex
+        align="center"
+        p="4"
+        mx="4"
+        borderRadius="lg"
+        role="group"
+        cursor="pointer"
+        _hover={{
+          bg: 'cyan.400',
+          color: 'white',
+        }}
+        {...rest}>
+        {icon && (
+          <Icon
+            mr="4"
+            fontSize="16"
+            _groupHover={{
+              color: 'white',
+            }}
+            as={icon}
+          />
+        )}
+        {children}
+      </Flex>
+    </Link>
+  );
+};
