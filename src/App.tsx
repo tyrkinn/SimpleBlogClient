a7a66db2ae28c8b8de82869c20c90a2a8072d2d8
tyrkinn <nikemad61@gmail.com>
2022-02-17 00:28:34 +0300

    Change App.tsx content to Fragment

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,6 @@
 import * as React from 'react';
-import {Center, Text, VStack} from '@chakra-ui/react';
-import {ColorModeSwitcher} from './components/shared/ColorModeSwitcher';
+import {Fragment} from 'react';
 
 export const App = () => (
-  <Center height={'100vh'}>
-    <VStack>
-      <Text fontSize={'xl'}>Hello World!</Text>
-      <ColorModeSwitcher/>
-    </VStack>
-  </Center>
+  <Fragment/>
 );
