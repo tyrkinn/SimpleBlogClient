d869248932a884baed0bd7bdebdd19c525dd712e
tyrkinn <nikemad61@gmail.com>
2022-02-16 22:21:52 +0300

    Move ChakraProvider in separated hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import * as React from 'react';
-import {Center, ChakraProvider, Text, theme, VStack} from '@chakra-ui/react';
+import {Center, Text, VStack} from '@chakra-ui/react';
 import {ColorModeSwitcher} from './components/shared/ColorModeSwitcher';
 
 export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Center height={'100vh'}>
-      <VStack>
-        <Text fontSize={'xl'}>Hello World!</Text>
-        <ColorModeSwitcher/>
-      </VStack>
-    </Center>
-  </ChakraProvider>
+  <Center height={'100vh'}>
+    <VStack>
+      <Text fontSize={'xl'}>Hello World!</Text>
+      <ColorModeSwitcher/>
+    </VStack>
+  </Center>
 );
