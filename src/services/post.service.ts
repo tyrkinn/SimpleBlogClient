e3768bb2a2e628d83dfaf6b245132405373d9f26
tyrkinn <nikemad61@gmail.com>
2022-02-18 18:47:58 +0300

    Change service interfaces
    Change IUserService type to interface
    Update some methods in interfaces to return void

--- src/services/post.service.ts
+++ src/services/post.service.ts
@@ -3,6 +3,6 @@ import {Post} from 'types/post.interface';
 import {Comment} from 'types/comment.interface';
 
 export interface IPostService extends ICurlService<Post>{
-  addComment(postId: number, comment: Comment): Comment;
-  deleteComment(id: number): Comment;
+  addComment(postId: number, comment: Comment): void;
+  deleteComment(id: number): void;
 }
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,4 +1,7 @@
 import {ICurlService} from '../types/curl-service.interface';
 import {User} from '../types/user.interface';
 
-export type IUserService = ICurlService<User>;
+export interface IUserService extends ICurlService<User>{
+  starPost(userId: number, postId: number): void;
+  unstarPost(userId: number, postId: number): void;
+}
