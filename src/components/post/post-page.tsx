a0da95cc073d309db91c179f1d15cf535f46a004
tyrkinn <nikemad61@gmail.com>
2022-02-18 19:56:14 +0300

    Create PostPage component

---
+++ src/components/post/post-page.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {IPost} from '../../types/post.interface';
+import {Box, Heading, Image, Stack} from '@chakra-ui/react';
+import ReactMarkdown from 'react-markdown';
+import ChakraUIRenderer from 'chakra-ui-markdown-renderer';
+
+export const PostPage: React.FC<{post: IPost}> = ({post}) => {
+  return (
+    <Box mt={10}>
+      <Heading>{post.title}</Heading>
+      <Stack mt={10}>
+        <Image height={400} src={post.imageLink}/>
+        <Box mt={10}>
+          <ReactMarkdown components={ChakraUIRenderer()}>
+            {post.markdownBody}
+          </ReactMarkdown>
+        </Box>
+      </Stack>
+    </Box>
+  );
+};
