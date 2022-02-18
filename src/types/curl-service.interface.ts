0b0f7e1fd6416902935fb36be5bb8c898a7b2802
tyrkinn <nikemad61@gmail.com>
2022-02-18 18:49:49 +0300

    Update curl-service interface
    Change create, update and delete methods to return void

--- src/types/curl-service.interface.ts
+++ src/types/curl-service.interface.ts
@@ -1,7 +1,7 @@
 export interface ICurlService<T> {
   getAll(): T[];
   getOne(id: number): T;
-  create(model: T): T;
-  update(id: number): T;
-  delete(id: number): T;
+  create(model: T): void;
+  update(id: number): void;
+  delete(id: number): void;
 }
