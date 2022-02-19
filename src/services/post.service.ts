30d49afa44fbd078f690161582f45096b80f13f6
tyrkinn <nikemad61@gmail.com>
2022-02-19 19:32:29 +0300

    Change services signature
    Now PostService recieves token on secure routes
    UserService uses DTOs to login and register
    UserService uses token for startPost and unstarPost methods

--- src/services/post.service.ts
+++ src/services/post.service.ts
@@ -1,13 +1,17 @@
-import {ICurlService} from 'types/curl-service.interface';
 import {IPost} from 'types/post.interface';
 import {IComment} from 'types/comment.interface';
 import {client} from './axios-client';
 import {ServerError} from '../types/server-error.interface';
 import {processServiceError} from '../utils/processServiceError';
 
-export interface IPostService extends ICurlService<IPost>{
-  addComment(postId: number, comment: IComment): Promise<IComment | ServerError>;
-  deleteComment(postId: number, commentId: number): Promise<IComment | ServerError>;
+export interface IPostService{
+  addComment(postId: number, comment: IComment, token: string): Promise<IComment | ServerError>;
+  deleteComment(postId: number, commentId: number, token: string): Promise<IComment | ServerError>;
+  getAll(): Promise<IPost[] | ServerError>;
+  getOne(id: number): Promise<IPost | ServerError>;
+  create(model: IPost, token: string): Promise<IPost | ServerError>;
+  update(id: number, post: IPost, token: string): Promise<IPost | ServerError>;
+  delete(id: number, token: string): Promise<IPost | ServerError>;
 }
 
 class PostService implements IPostService {
@@ -15,43 +19,6 @@ class PostService implements IPostService {
     try {
       const response = await client.get<IPost[]>('/posts');
       return response.data;
-    } catch (e) {
-      return processServiceError(e);
-    }
-  }
-
-  async addComment(postId: number, comment: IComment): Promise<IComment | ServerError> {
-    try {
-      const response = await client.post<IComment>(`/posts/${postId}/comments`, comment);
-      return response.data;
-    } catch (e) {
-      return processServiceError(e);
-    }
-  }
-
-  async create(model: IPost): Promise<ServerError | IPost> {
-    try {
-      const resonse = await client.post<IPost>('/posts', model);
-      return resonse.data;
-    } catch (e) {
-      return processServiceError(e);
-    }
-  }
-
-  async delete(id: number): Promise<ServerError | IPost> {
-    try {
-      const response = await client.delete<IPost>(`/posts/${id}`);
-      return response.data;
-    } catch (e) {
-      return processServiceError(e);
-    }
-  }
-
-  async deleteComment(postId: number, commentId: number): Promise<IComment | ServerError> {
-    try {
-      const resonse = await client
-          .delete<IComment>(`/posts/${postId}/comments/${commentId}`);
-      return resonse.data;
     } catch (e) {
       return processServiceError(e);
     }
@@ -67,10 +34,68 @@ class PostService implements IPostService {
     }
   }
 
-  async update(id: number): Promise<ServerError | IPost> {
+  async create(model: IPost, token: string): Promise<ServerError | IPost> {
+    try {
+      const resonse = await client.post<IPost>('/posts', model, {
+        headers: {
+          'Authorization': `Bearer ${token}`,
+        },
+      });
+      return resonse.data;
+    } catch (e) {
+      return processServiceError(e);
+    }
+  }
+
+  async delete(id: number, token: string): Promise<ServerError | IPost> {
+    try {
+      const response = await client.delete<IPost>(`/posts/${id}`, {
+        headers: {
+          'Authorization': `Bearer ${token}`,
+        },
+      });
+      return response.data;
+    } catch (e) {
+      return processServiceError(e);
+    }
+  }
+
+  async addComment(postId: number, comment: IComment, token: string): Promise<IComment | ServerError> {
+    try {
+      const response = await client.post<IComment>(`/posts/${postId}/comments`, comment, {
+        headers: {
+          'Authorization': `Bearer ${token}`,
+        },
+      });
+      return response.data;
+    } catch (e) {
+      return processServiceError(e);
+    }
+  }
+
+  async deleteComment(postId: number, commentId: number, token: string): Promise<IComment | ServerError> {
+    try {
+      const resonse = await client
+          .delete<IComment>(`/posts/${postId}/comments/${commentId}`, {
+            headers: {
+              'Authorization': `Bearer ${token}`,
+            },
+          });
+      return resonse.data;
+    } catch (e) {
+      return processServiceError(e);
+    }
+  }
+
+
+  async update(id: number, post: IPost, token: string): Promise<ServerError | IPost> {
     try {
       const response = await client
-          .put<IPost>(`/posts/${id}`);
+          .put<IPost>(`/posts`, post, {
+            headers: {
+              'Authorization': `Bearer ${token}`,
+            },
+          });
       return response.data;
     } catch (e) {
       return processServiceError(e);
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,7 +1,11 @@
-import {ICurlService} from '../types/curl-service.interface';
-import {IUser} from '../types/user.interface';
+import {IPost} from '../types/post.interface';
+import {ServerError} from '../types/server-error.interface';
+import {RegisterUserDto} from '../types/dto/register-user.dto';
+import {LoginUserDto} from '../types/dto/login-user.dto';
 
-export interface IUserService extends ICurlService<IUser>{
-  starPost(userId: number, postId: number): void;
-  unstarPost(userId: number, postId: number): void;
+export interface IUserService {
+  login(user: LoginUserDto): Promise<{token: string} | ServerError>
+  register(user: RegisterUserDto): Promise<{token: string} | ServerError>
+  starPost(postId: number, token: string): Promise<IPost | ServerError>;
+  unstarPost(postId: number, token: string): Promise<IPost | ServerError>;
 }
