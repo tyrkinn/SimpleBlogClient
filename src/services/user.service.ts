51c59e5f261f902db6cf45e347e4eb7ffa98b4bc
tyrkinn <nikemad61@gmail.com>
2022-02-19 19:35:31 +0300

    Minimal style changes

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -4,8 +4,8 @@ import {RegisterUserDto} from '../types/dto/register-user.dto';
 import {LoginUserDto} from '../types/dto/login-user.dto';
 
 export interface IUserService {
-  login(user: LoginUserDto): Promise<{token: string} | ServerError>
-  register(user: RegisterUserDto): Promise<{token: string} | ServerError>
+  login(user: LoginUserDto): Promise<{token: string} | ServerError>;
+  register(user: RegisterUserDto): Promise<{token: string} | ServerError>;
   starPost(postId: number, token: string): Promise<IPost | ServerError>;
   unstarPost(postId: number, token: string): Promise<IPost | ServerError>;
 }
