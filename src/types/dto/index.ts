2a67a25fec20a07f35cd2364f1b48595e84e7a8b
tyrkinn <nikemad61@gmail.com>
2022-02-27 00:29:15 +0300

    Add index files with exports for types and types/dto

---
+++ src/types/dto/index.ts
@@ -0,0 +1,3 @@
+export type {LoginUserDto} from './login-user.dto';
+export type {RegisterUserDto} from './register-user.dto';
+export type {UpdateUserDto} from './update-user.dto';
---
+++ src/types/index.ts
@@ -0,0 +1,5 @@
+export type {IComment} from './comment.interface';
+export type {IPost} from './post.interface';
+export type {IUser} from './user.interface';
+export type {ServerError} from './server-error.interface';
+export type {TokenResponse} from './token-response.interface';
