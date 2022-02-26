c8529fe6f898bec452f7a5c84f8a901393bc6415
tyrkinn <nikemad61@gmail.com>
2022-02-26 21:05:33 +0300

    Change post signature
    Add 'tags' field to store post tags
    Delete 'createdBy' field

--- src/types/post.interface.ts
+++ src/types/post.interface.ts
@@ -1,10 +1,8 @@
-import {IUser} from './user.interface';
-
 export interface IPost {
   id?: number;
   title: string;
   markdownBody: string;
   imageLink: string;
-  createdBy: IUser;
   createdAt: string;
+  tags: string[];
 }
